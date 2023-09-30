dc7366425e5600b69d4d54501adc3699c562c8ad
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-09-30 17:38:07 +0200

    Add files via upload

--- gen.zig
+++ gen.zig
@@ -12,7 +12,7 @@ const AspectRatio: f32 = 16.0 / 9.0;
 const WIDTH: u32 = 900;
 pub const ImageProperties: def.IMG = img.PropIMG(WIDTH, AspectRatio);
 
-fn UsedThread() usize {
+fn usedThread() usize {
     const UsedCoreErr = thr.getCpuCount();
     var UsedCore = UsedCoreErr catch |err| {
         std.debug.print("ERROR IN GETTING CPU COUNT => {}\n", .{err});
@@ -22,11 +22,11 @@ fn UsedThread() usize {
 }
 
 fn thread(COLOR: def.COL) void {
-    var UsedCore: usize = UsedThread();
+    var UsedCore: usize = usedThread();
     const SpawnConfig = thr.SpawnConfig{
         .stack_size = 4096,
     };
-    var i: u8 = 0;
+    var i: usize = 0;
     var ThreadArray: [MAX_THREADS]thr = undefined;
     while (i < UsedCore) {
         var ThreadArrayERR = thr.spawn(SpawnConfig, col.color, .{COLOR});
@@ -36,18 +36,18 @@ fn thread(COLOR: def.COL) void {
         };
         i += 1;
     }
-    var j: u8 = 0;
+    var j: usize = 0;
     while (j < UsedCore) {
-        if (@TypeOf(ThreadArray[i]) == @TypeOf(thr)) {
+        if (@TypeOf(ThreadArray[j]) == @TypeOf(thr)) {
             thr.join(ThreadArray[j]);
         }
-        i += 1;
+        j += 1;
     }
 }
 
 pub fn render() void {
     const COLOR = def.COL{ .R = 123, .G = 89, .B = 224 };
-    const UsedCore = UsedThread();
+    const UsedCore = usedThread();
     //file head
     sto.print("P3\n{} {}\n255\n", .{ ImageProperties.WIDTH, ImageProperties.HEIGHT }) catch |err| {
         std.debug.print("Oops, there seems to be an error... {}\n", .{err});
