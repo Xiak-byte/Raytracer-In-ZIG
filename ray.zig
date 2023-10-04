0cd938b347649269be4f94698ebac7a64c0ec74d
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-10-04 15:50:04 +0200

    Add files via upload

--- ray.zig
+++ ray.zig
@@ -2,17 +2,6 @@ const std = @import("std");
 const def = @import("def.zig");
 const pop = @import("pop.zig");
 
-pub fn SphereRayIntersection(sphere: def.SPR, R: def.RAY) bool {
-    var v: def.P3D = pop.Sub(sphere.POS, R.POS);
-    var b: f32 = pop.Dot(R.DIR, v);
-    var discriminant: f32 = b * b - pop.Dot(v, v) + sphere.RAD * sphere.RAD;
-    if (discriminant > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 pub fn SphereIntersectionRay(sphere: def.SPR, R: def.RAY) bool {
     const a: f32 = pop.Dot(R.DIR, R.DIR);
     const v: def.P3D = pop.Sub(sphere.POS, R.POS);
@@ -21,7 +10,6 @@ pub fn SphereIntersectionRay(sphere: def.SPR, R: def.RAY) bool {
 
     const discriminant: f32 = b * b - 4.0 * a * c;
     if (discriminant > 0) {
-        std.debug.print("Intersection\n", .{});
         return true;
     } else {
         return false;
