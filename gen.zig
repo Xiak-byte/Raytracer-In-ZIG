f4e959e4e26c64d9bfa2a90ca0760a2dd87bf98b
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-10-04 11:05:32 +0200

    Add files via upload

--- gen.zig
+++ gen.zig
@@ -11,14 +11,14 @@ const alloc = @import("std").heap.page_allocator;
 const COLOR = def.P3D{ .X = 123, .Y = 89, .Z = 224 };
 const COLOR_SPHERE = def.P3D{ .X = 255, .Y = 255, .Z = 0 };
 const AspectRatio: f32 = 16.0 / 9.0;
-const WIDTH: u32 = 900;
+const WIDTH: u32 = 256;
 pub const ImageProperties: def.IMG = img.PropIMG(WIDTH, AspectRatio);
 pub var CAMERA = def.RAY{
     .POS = def.P3D{ .X = 0, .Y = 10, .Z = 0 },
     .DIR = def.P3D{ .X = -@as(f64, WIDTH) / 2.0, .Y = @as(f64, ImageProperties.HEIGHT) / 2.0, .Z = 0 },
 };
-const SPHERE_POS = def.P3D{ .X = 0, .Y = 10, .Z = 2 };
-const SPHERE = def.SPR{ .COL = COLOR_SPHERE, .POS = SPHERE_POS, .RAD = 0.2 };
+const SPHERE_POS = def.P3D{ .X = 0, .Y = 10, .Z = 20 };
+const SPHERE = def.SPR{ .COL = COLOR_SPHERE, .POS = SPHERE_POS, .RAD = 2 };
 
 pub fn render() void {
     //file head
@@ -30,12 +30,16 @@ pub fn render() void {
     while (i < ImageProperties.HEIGHT) {
         std.debug.print("ScanLines remaining {}/{}\n", .{ (ImageProperties.HEIGHT - i), ImageProperties.HEIGHT });
         while (j < ImageProperties.WIDTH) {
+            var k: f32 = 0;
             var R: def.RAY = CAMERA;
             R.DIR.X += @floatFromInt(j);
             R.DIR.Y += @floatFromInt(i);
-            if (ray.SphereIntersectionRay(SPHERE, R)) {
-                col.color(COLOR_SPHERE);
-            } else {
+            while (k < 100) {
+                if (ray.SphereIntersectionRay(SPHERE, R)) {
+                    col.color(COLOR_SPHERE);
+                    break;
+                }
+                R.DIR.Z += 1;
                 col.color(COLOR);
             }
             j += 1;
