7a85595836ba11100c384c4486691996d4d5789c
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-10-04 10:35:44 +0200

    Add files via upload

--- gen.zig
+++ gen.zig
@@ -1,49 +1,45 @@
 const std = @import("std");
-const gen = @import("gen.zig");
 const def = @import("def.zig");
-const col = @import("col.zig");
-const scn = @import("scn.zig");
 const img = @import("img.zig");
+const col = @import("col.zig");
 const pop = @import("pop.zig");
+const ray = @import("ray.zig");
 const sto = @import("std").io.getStdOut().writer();
 const thr = @import("std").Thread;
 const alloc = @import("std").heap.page_allocator;
 
 const COLOR = def.P3D{ .X = 123, .Y = 89, .Z = 224 };
+const COLOR_SPHERE = def.P3D{ .X = 255, .Y = 255, .Z = 0 };
 const AspectRatio: f32 = 16.0 / 9.0;
 const WIDTH: u32 = 900;
 pub const ImageProperties: def.IMG = img.PropIMG(WIDTH, AspectRatio);
 pub var CAMERA = def.RAY{
     .POS = def.P3D{ .X = 0, .Y = 10, .Z = 0 },
-    .DIR = def.P3D{ .X = -@as(f64, WIDTH) / 2.0, .Y = @as(f64, ImageProperties.HEIGHT) / 2.0, .Z = 100 },
+    .DIR = def.P3D{ .X = -@as(f64, WIDTH) / 2.0, .Y = @as(f64, ImageProperties.HEIGHT) / 2.0, .Z = 0 },
 };
-const FOV: f32 = 120;
-const FOV_RAD: f32 = FOV * (std.math.pi / 180.0);
-const STEP: f32 = 2.0 * std.math.tan(FOV_RAD / 2.0) / @as(f32, ImageProperties.WIDTH);
-const PIXEL_GRID_W = def.P3D{ .X = STEP, .Y = 0, .Z = 0 };
-const PIXEL_GRID_H = def.P3D{ .X = 0, .Y = STEP, .Z = 0 };
+const SPHERE_POS = def.P3D{ .X = 0, .Y = 5, .Z = 50 };
+const SPHERE = def.SPR{ .COL = COLOR_SPHERE, .POS = SPHERE_POS, .RAD = 1 };
 
 pub fn render() void {
     //file head
     sto.print("P3\n{} {}\n255\n", .{ ImageProperties.WIDTH, ImageProperties.HEIGHT }) catch |err| {
         std.debug.print("Oops, there seems to be an error... {}\n", .{err});
     };
-    var i: usize = 0;
-    var j: usize = 0;
+    var i: u32 = 0;
+    var j: u32 = 0;
     while (i < ImageProperties.HEIGHT) {
         std.debug.print("ScanLines remaining {}/{}\n", .{ (ImageProperties.HEIGHT - i), ImageProperties.HEIGHT });
         while (j < ImageProperties.WIDTH) {
-            if (scn.IntersectSphere(CAMERA, scn.SPHERE)) {
-                col.color(scn.SPHERE.COL);
+            var R: def.RAY = CAMERA;
+            R.DIR.X += @floatFromInt(j);
+            R.DIR.Y += @floatFromInt(i);
+            if (ray.SphereRayIntersection(SPHERE, R)) {
+                col.color(COLOR_SPHERE);
             } else {
                 col.color(COLOR);
             }
-            CAMERA.DIR = pop.Add(CAMERA.DIR, PIXEL_GRID_W);
             j += 1;
         }
-
-        CAMERA.DIR = def.P3D{ .X = -@as(f64, WIDTH) / 2.0, .Y = @as(f64, ImageProperties.HEIGHT) / 2.0, .Z = 100 };
-        CAMERA.DIR = pop.Sub(CAMERA.DIR, PIXEL_GRID_H);
         j = 0;
         i += 1;
     }
--- ray.zig
+++ ray.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const def = @import("def.zig");
-const scn = @import("scn.zig");
 const pop = @import("pop.zig");
 
-var EYE: def.P3D = pop.Sub(scn.CAMERA.VEC, scn.CAMERA.POS);
+pub fn SphereRayIntersection(sphere: def.SPR, R: def.RAY) bool {
+    var v: def.P3D = pop.Sub(sphere.POS, R.POS);
+    var b: f32 = pop.Dot(R.DIR, v);
+    var discriminant: f32 = b * b - pop.Dot(v, v) + sphere.RAD * sphere.RAD;
+    if (discriminant > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
