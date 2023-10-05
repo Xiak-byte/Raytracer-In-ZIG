f6528c4e71bbb947c40baa64c492c9b832cf3b63
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-10-05 14:18:53 +0200

    Delete img.zig

--- img.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const def = @import("def.zig");
-
-pub fn PropIMG(WIDTH: u32, AspectRatio: f32) def.IMG {
-    var HEIGHT: u32 = @intFromFloat(@as(f32, WIDTH) / AspectRatio);
-    var ImgPropreties = def.IMG{ .WIDTH = WIDTH, .HEIGHT = HEIGHT };
-    return ImgPropreties;
-}
