cc941da8ba7d442f9c7d8d29bcd0b34ab1d904f3
Xiak-byte <71687886+Xiak-byte@users.noreply.github.com>
2023-10-05 15:00:45 +0200

    Add files via upload

--- gen.zig
+++ gen.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 const def = @import("def.zig");
 const ray = @import("ray.zig");
 const sto = @import("std").io.getStdOut().writer();
-const thr = @import("std").Thread;
-const alloc = @import("std").heap.page_allocator;
 
+//some important variable
 const COLOR = def.P3D{ .X = 123, .Y = 89, .Z = 224 };
 const COLOR_SPHERE = def.P3D{ .X = 255, .Y = 255, .Z = 0 };
 const AspectRatio: f32 = 16.0 / 9.0;
 const WIDTH: u32 = 256;
 
+//image properties, height, width
 fn PropIMG(W: u32, ASPECT_RATIO: f32) def.IMG {
     var HEIGHT: u32 = @intFromFloat(@as(f32, W) / ASPECT_RATIO);
     var ImgPropreties = def.IMG{ .WIDTH = W, .HEIGHT = HEIGHT };
@@ -17,13 +17,18 @@ fn PropIMG(W: u32, ASPECT_RATIO: f32) def.IMG {
 }
 
 const ImageProperties: def.IMG = PropIMG(WIDTH, AspectRatio);
+
+//camera position and direction
 pub var CAMERA = def.RAY{
-    .POS = def.P3D{ .X = 0, .Y = 10, .Z = 0 },
-    .DIR = def.P3D{ .X = 0, .Y = 0, .Z = 0 },
+    .POS = def.P3D{ .X = 0, .Y = 0, .Z = 0 },
+    .DIR = def.P3D{ .X = -@as(f32, ImageProperties.WIDTH) / 2, .Y = @as(f32, ImageProperties.HEIGHT) / 2, .Z = 0 },
 };
-const SPHERE_POS = def.P3D{ .X = 0, .Y = 10, .Z = 2 };
+
+//sphere position, radius and color
+const SPHERE_POS = def.P3D{ .X = 0, .Y = 0, .Z = 5 };
 const SPHERE = def.SPR{ .COL = COLOR_SPHERE, .POS = SPHERE_POS, .RAD = 2 };
 
+//function to print the current pixel color
 fn color(Color: def.P3D) void {
     var R: usize = @intFromFloat(Color.X);
     var G: usize = @intFromFloat(Color.Y);
@@ -33,11 +38,13 @@ fn color(Color: def.P3D) void {
     };
 }
 
+//function to render the ppm file
 pub fn render() void {
-    //file head
+    //file header
     sto.print("P3\n{} {}\n255\n", .{ ImageProperties.WIDTH, ImageProperties.HEIGHT }) catch |err| {
         std.debug.print("Oops, there seems to be an error... {}\n", .{err});
     };
+    //loop to iterate over each pixels
     var i: u32 = 0;
     var j: u32 = 0;
     while (i < ImageProperties.HEIGHT) {
@@ -46,8 +53,9 @@ pub fn render() void {
             var R: def.RAY = CAMERA;
             R.DIR.X += @floatFromInt(j);
             R.DIR.Y += @floatFromInt(i);
+            //check for intersection
             if (ray.SphereIntersectionRay(SPHERE, R)) {
-                color(COLOR_SPHERE);
+                color(SPHERE.COL);
             } else {
                 color(COLOR);
             }
